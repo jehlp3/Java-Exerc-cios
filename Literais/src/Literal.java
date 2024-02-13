afd5645361040ddd3ad35cda4d7a86aa78f966af
jehlp3 <jessicalacerdap@hotmail.com>
2024-02-12 21:11:55 -0300

    Introdução Java

---
+++ Literais/src/Literal.java
@@ -0,0 +1,16 @@
+
+public class Literal {
+
+	public static void main(String[] args) {
+		int numero = 10;
+		String cadeiaCaracteres = "Meu Curso de Java Fundamentos";
+		char caracter = 'S';
+		boolean certoOuErrado = true;
+		
+		System.out.println("O valor de número é " + numero);
+		System.out.println("O valor de cadeiaCaracteres é " + cadeiaCaracteres	);
+		System.out.println("O valor de caracter é " + caracter);
+		System.out.println("O valor de certoOuErrado é " + certoOuErrado);
+	}
+
+}
---
+++ TiposDeDados/src/TipoDeDados.java
@@ -0,0 +1,20 @@
+
+public class TipoDeDados {
+
+	public static void main(String[] args) {
+		int numero;
+		float decimalCurto;
+		double decimalLongo;
+		char caracter;
+		String cadeiaCaracteres;
+
+		numero = 10;
+		decimalCurto = 10.5f;
+		decimalLongo = 20.5;
+		caracter = 'A';
+		cadeiaCaracteres = "Meu curso Java Fundamentos ";
+		
+		
+	}
+
+}
