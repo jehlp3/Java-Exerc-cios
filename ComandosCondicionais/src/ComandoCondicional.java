6ffcf2dcf20ac1966800062d36c7a81362f15558
jehlp3 <jessicalacerdap@hotmail.com>
2024-02-14 10:50:51 -0300

    Atualização

---
+++ ComandosCondicionais/src/ComandoCondicional.java
@@ -0,0 +1,17 @@
+
+public class ComandoCondicional {
+
+	public static void main(String[] args) {
+		//IF
+		int num1 = 30, num2 = 30;
+		if (num1 > num2) {
+			System.out.println("O número "+num1+" é maior que o número "+num2);
+		} else if (num1 > num2){
+			System.out.println("O número "+num1+" é menor que o número "+num2);	
+		} else {
+			System.out.println("Os números "+num1+" e "+num2+" são iguais.");
+		}
+
+	}
+
+}
