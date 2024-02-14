45d4e2d8d414525a3d7ded3ad1f1e5237391203b
jehlp3 <jessicalacerdap@hotmail.com>
2024-02-14 11:14:13 -0300

    Atualização

--- ComandosCondicionais/src/ComandoCondicional.java
+++ ComandosCondicionais/src/ComandoCondicional.java
@@ -12,6 +12,29 @@ public class ComandoCondicional {
 			System.out.println("Os números "+num1+" e "+num2+" são iguais.");
 		}
 
+		//Switch
+		char varOpcao = '2';
+		
+		switch(varOpcao) {
+		case '1': {
+			System.out.println("1 - Chame programa de Inclusão");
+			break; //não esqueça do break!
+		}
+		case '2': {
+			System.out.println("2 - Chame programa de Alteração");
+			break;
+		}
+		case '3': {
+			System.out.println("3 - Chame programa de Consulta");
+			break;
+		}
+		case '4': {
+			System.out.println("4 - Chame programa de Exclusão");
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("ERRO! Opção inválida "+varOpcao+", Redigite!");
+		
 	}
 
-}
+}}
